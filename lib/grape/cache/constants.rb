ba8e7a741c28a2027d973ddd362a50c64714b537
wolfemm <mike.wolfe.millard@gmail.com>
2018-07-08 00:07:31 -0700

    Add constants for all Cache-Control directives

--- lib/grape/cache/constants.rb
+++ lib/grape/cache/constants.rb
@@ -2,9 +2,23 @@
 
 module Grape
   module Cache
+    # Cacheability
     PUBLIC = "public"
     PRIVATE = "private"
     NO_CACHE = "no-cache"
     ONLY_IF_CACHED = "only-if-cached"
+
+    # Expiration
+    MAX_AGE = "max-age"
+    S_MAXAGE = "s-maxage"
+    MAX_STALE = "max-stale"
+    MIN_FRESH = "min-fresh"
+    STALE_WHILE_REVALIDATE = "stale-while-revalidate"
+    STALE_IF_ERROR = "stale-if-error"
+
+    # Revalidation and reloading
+    MUST_REVALIDATE = "must-revalidate"
+    PROXY_REVALIDATE = "proxy-revalidate"
+    IMMUTABLE = "immutable"
   end
 end
