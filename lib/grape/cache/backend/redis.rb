eeed38fe634ca7c6160ade718f569c23942a0325
wolfemm <mike.wolfe.millard@gmail.com>
2018-07-07 22:51:48 -0700

    Cache values as JSON instead of marshaling

--- lib/grape/cache/backend/redis.rb
+++ lib/grape/cache/backend/redis.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'memory'
+require_relative "memory"
+require_relative "cache_entry_metadata"
+
 module Grape
   module Cache
     module Backend
       class Redis < Memory
         def initialize(redis_connection)
-          raise 'Expecting redis connection here' unless redis_connection
+          raise "Expecting redis connection here" unless redis_connection
           @storage = redis_connection
         end
 
@@ -14,7 +16,13 @@ module Grape
         # @param response[Rack::Response]
         # @param metadata[Grape::Cache::Backend::CacheEntryMetadata] Entry metadata
         def store(key, response, metadata)
-          args = [key, 'status', response.status.to_s, 'headers', Marshal.dump(response.headers), 'body', Marshal.dump(response.body), 'metadata', Marshal.dump(metadata)]
+          args = [
+            key,
+            "status", response.status.to_s,
+            "headers", MultiJson.dump(response.headers),
+            "body", MultiJson.dump(response.body),
+            "metadata", MultiJson.dump(metadata)
+          ]
           if metadata.expire_at
             storage.multi
             storage.hmset(*args)
@@ -24,16 +32,19 @@ module Grape
             storage.hmset(*args)
           end
         end
+
         def fetch(key)
-          status, headers, body = storage.hmget(key, 'status', 'headers', 'body')
-          Rack::Response.new(Marshal.load(body), status.to_i, Marshal.load(headers))
+          status, headers, body = storage.hmget(key, "status", "headers", "body")
+          Rack::Response.new(MultiJson.load(body), status.to_i, MultiJson.load(headers))
         rescue
           nil
         end
 
         # @param key[String] Cache key
         def fetch_metadata(key)
-          Marshal.load(storage.hget(key, 'metadata'))
+          Grape::Cache::Backend::CacheEntryMetadata.new(
+            MultiJson.load(storage.hget(key, "metadata"))
+          )
         rescue
           nil
         end
@@ -43,6 +54,7 @@ module Grape
         end
 
         private
+
         def storage
           @_storage ||= @storage.respond_to?(:arity) ? @storage.call : @storage
         end
