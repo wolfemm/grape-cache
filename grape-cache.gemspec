f77cb837383c108d79e42a4ff6f5c43d71203622
wolfemm <mike.wolfe.millard@gmail.com>
2018-07-08 06:39:52 -0700

    Further reduce backend memory usage by compressing larger payloads and using shorter keys

--- grape-cache.gemspec
+++ grape-cache.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "grape", "~> 1"
   spec.add_runtime_dependency "murmurhash3"
   spec.add_runtime_dependency "activesupport"
+  spec.add_runtime_dependency "zstd-ruby"
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
--- lib/grape/cache/backend/redis.rb
+++ lib/grape/cache/backend/redis.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "zstd-ruby"
+
 require_relative "memory"
 require_relative "cache_entry_metadata"
 
@@ -7,6 +9,16 @@ module Grape
   module Cache
     module Backend
       class Redis < Memory
+        ZIP_THRESHOLD = 10.kilobytes
+        STATUS_KEY = "s"
+        HEADERS_KEY = "h"
+        METADATA_KEY = "m"
+        BODY_KEY = "b"
+        COMPRESSION_KEY = "c"
+
+        FLAG_BODY = 1
+        FLAG_HEADERS = 2
+
         def initialize(redis_connection)
           raise "Expecting redis connection here" unless redis_connection
           @storage = redis_connection
@@ -16,15 +28,26 @@ module Grape
         # @param response[Rack::Response]
         # @param metadata[Grape::Cache::Backend::CacheEntryMetadata] Entry metadata
         def store(key, response, metadata)
+          # Using #as_json to avoid quote escaping since this won't be parsed before being sent
+          json_body = response.body.first
+          header_json = MultiJson.dump(response.headers)
+
+          compress_body = json_body.bytesize >= ZIP_THRESHOLD
+          compress_headers = header_json.bytesize >= ZIP_THRESHOLD
+
+          compression_flags = 0
+          compression_flags |= FLAG_BODY if compress_body
+          compression_flags |= FLAG_HEADERS if compress_headers
+
           args = [
             key,
-            "status", response.status.to_s,
-            "headers", MultiJson.dump(response.headers),
-            "metadata", MultiJson.dump(metadata),
-
-            # Using #as_json to avoid quote escaping since this won't be parsed before being sent
-            "body", response.body.as_json,
+            COMPRESSION_KEY, compression_flags.to_s,
+            STATUS_KEY,      response.status.to_s,
+            METADATA_KEY,    MultiJson.dump(metadata),
+            HEADERS_KEY,     compress_if(compress_headers, header_json),
+            BODY_KEY,        compress_if(compress_body, json_body),
           ]
+
           if metadata.expire_at
             storage.multi
             storage.hmset(*args)
@@ -35,8 +58,23 @@ module Grape
           end
         end
 
+        def compress_if(condition, value)
+          condition ? Zstd.compress(value) : value
+        end
+
+        def decompress_if(condition, value)
+          condition ? Zstd.decompress(value) : value
+        end
+
         def fetch(key)
-          status, headers, body = storage.hmget(key, "status", "headers", "body")
+          status, headers, body, compression_flags = storage.hmget(
+            key, STATUS_KEY, HEADERS_KEY, BODY_KEY, COMPRESSION_KEY
+          )
+
+          compression_flags = compression_flags.to_i
+          body = decompress_if(compression_flags & FLAG_BODY != 0, body)
+          headers = decompress_if(compression_flags & FLAG_HEADERS != 0, headers)
+
           Rack::Response.new(body, status.to_i, MultiJson.load(headers))
         rescue
           nil
@@ -45,7 +83,7 @@ module Grape
         # @param key[String] Cache key
         def fetch_metadata(key)
           Grape::Cache::Backend::CacheEntryMetadata.new(
-            MultiJson.load(storage.hget(key, "metadata"))
+            MultiJson.load(storage.hget(key, METADATA_KEY))
           )
         rescue
           nil
